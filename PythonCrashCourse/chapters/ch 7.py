5ad431e9475511d56503b989224e8decbb9e3807
Osama Hatem <99215915+osama1hatem2abdelkreem3@users.noreply.github.com>
2024-08-22 08:31:59 +0300

    Add files via upload

---
+++ PythonCrashCourse/chapters/ch 7.py
@@ -0,0 +1,51 @@
+#input and while loops
+#input() function is used to get the input from the user
+# message=input('please enter your name!!\n')
+# print(f"your name is {message.title()}")
+
+
+
+# message='welcome to our website'
+# message+='\nplease enter your name\n'
+# name=input(message)
+# print(name.title())
+
+
+#<name > += <string>   is used to add a string to the variable
+
+
+
+# age=input('how old are you \n')
+# if  int(age)>18:
+#     print('you are old enough for  voting')
+
+# else:
+#     print('you are  not old enough for voting')
+
+
+#while loops
+# while <condition>:
+#     <body of the loop>
+#     <increment/decrement>
+# i=0
+# out=''
+# i=1
+# j=''
+# city=''
+# team_list=[]
+# a=0
+# while 1:
+#     j=input('enter name\n')
+#     a=input('\nenter age\n')
+#     team={'id':i,'name':j,'age':a}
+#     team_list.append(team)
+#     i=i+1
+#     out = input("\ndo you want to quit press q\n") 
+#     if out=='q':
+#         break
+
+# for show in team_list:
+#     print(f'{show}')    
+
+
+# # remember we can't append dictionary but we can append  list of dictionaries
